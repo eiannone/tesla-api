d8baa46b77cc5fb38cf271ee68755635c2a5ea09
Emanuele Iannone <emanuele@fondani.it>
2021-09-28 20:41:44 +0200

    Event Emitter and resubscribe delay

--- TeslaStream.js
+++ TeslaStream.js
@@ -1,14 +1,15 @@
 import WebSocket from 'ws';
+import {EventEmitter} from 'events';
 
 const CONNECTING = 1;
 const CONNECTED = 2;
 const CLOSING = 3;
 const CLOSED = 4;
 
-export default class TeslaStream {
+export default class TeslaStream extends EventEmitter {
     static DEFAULT_COLUMNS = ['elevation', 'est_heading', 'est_lat', 'est_lng', 'odometer', 'power', 'shift_state', 'speed', 'soc'];
 
-    constructor(logger = null, cb_inactive = null, cb_disconnects = null) {
+    constructor(logger = null) {
         this.log = logger || this.#internalLog;
         this.ws = null;
         this.state = CLOSED;
@@ -16,8 +17,6 @@ export default class TeslaStream {
         this.checkTimeout = null;
         this.timeouts = 0;
         this.disconnects = 0;
-        this.cbInactive = cb_inactive;
-        this.cbDisconnects = cb_disconnects;
         this.reconnect = true;
         this.tag = null;
         this.lastShiftState = null;
@@ -74,7 +73,7 @@ export default class TeslaStream {
         this.log("Stream connection timed out / " + this.timeouts, level);        
         if (this.timeouts % 3 == 0) { // Teslamate does it every 5th attempt
             this.log("Stream inactive!");
-            if (this.cbInactive != null) this.cbInactive();
+            this.emit('inactive');
         }
         this.#reconnect();
     }
@@ -89,24 +88,24 @@ export default class TeslaStream {
         }));        
     }
 
-    #disconnectResubscribe(tag, token, reason) {
+    #disconnectResubscribe(tag, token, reason, minDelay) {
         this.disconnects++;
         this.log(reason + ((this.disconnects == 1)? '' : ' / '+this.disconnects), (this.disconnects < 8)? 'debug' : 'info');
         clearTimeout(this.checkTimeout);
         if (this.disconnects % 10 == 0) {
             this.log("Too many disconnects!", "warn");
-            if (this.cbDisconnects != null) this.cbDisconnects();
+            this.emit('too-many-diconnects');
         }
         else {
             const ms = (this.lastShiftState != null && this.lastShiftState != "")?
                 this.#expBackOffMs(this.disconnects, 0, 8, 1.3) :
-                this.#expBackOffMs(this.disconnects, 30, 60); // Teslamate uses min 15, max 30
+                this.#expBackOffMs(this.disconnects, minDelay, 60); // Teslamate uses min 15, max 30
             this.log("Waiting for " + Math.round(ms / 1000) + " sec...");                            
             this.checkTimeout = setTimeout(_ => { this.#subscribe(tag, token); }, ms);
         }        
     }
 
-    connect(tag, token, columns = null, cb_data = null, cb_error = null) {
+    connect(tag, token, columns = null, resubscribeDelay = 30) {
         this.tag = tag.toString();
         if (columns != null) this.columns = columns;        
         let shiftStatePos = this.columns.indexOf('shift_state');
@@ -135,21 +134,21 @@ export default class TeslaStream {
                 this.state = CONNECTED;
             } else if (d.msg_type == 'data:update') {
                 let values = d.value.split(",");
-                if (cb_data != null) cb_data(values);
+                this.emit('stream', values);
                 this.timeouts = 0;
                 this.disconnects = 0;
                 if (shiftStatePos > -1) this.lastShiftState = values[shiftStatePos + 1];
             } else if (d.msg_type == 'data:error' && typeof d.error_type != "undefined") {
                 switch(d.error_type) {
                     case "vehicle_disconnected":
-                        this.#disconnectResubscribe(tag, token, "Vehicle disconnected");
+                        this.#disconnectResubscribe(tag, token, "Vehicle disconnected", resubscribeDelay);
                         break;
                     case "vehicle_error":
-                        this.#disconnectResubscribe(tag, token, "Vehicle error: " + d.value);
+                        this.#disconnectResubscribe(tag, token, "Vehicle error: " + d.value, resubscribeDelay);
                         break;
                     case "client_error":
                         this.log("Client error: " + d.value, "error");
-                        if (cb_error != null) cb_error(d.value);
+                        this.emit('error', d.value);
                         this.#reconnect();
                         break;
                     default:
@@ -158,7 +157,7 @@ export default class TeslaStream {
                 }                
             } else {
                 this.log("Unknown message: " + data, "error");
-                if (cb_error != null) cb_error(data);
+                this.emit('error', data);
             }
         });
         this.ws.on('error', (error) => {
@@ -174,7 +173,7 @@ export default class TeslaStream {
             if (this.reconnect) {
                 this.checkTimeout = setTimeout(() => { 
                     this.log("Reconnecting...");
-                    this.connect(tag, token, columns, cb_data, cb_error); 
+                    this.connect(tag, token, columns); 
                 }, 1000);
             }
         });
